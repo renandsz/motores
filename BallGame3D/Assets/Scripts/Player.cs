0f38f6e2a91c9a37f10d7764ef45640f299d6149
Renan Santos <renandsz@hotmail.com>
2023-10-02 14:37:59 -0300

    camera follow

---
+++ BallGame3D/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public int velocidade = 10;    
+    private Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        TryGetComponent(out rb);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal"); //-1 esquerda,0 nada, 1 direita
+        float v = Input.GetAxis("Vertical");// -1 pra tras, 0 nada, 1 pra frente
+        
+        Vector3 direcao = new Vector3(h, 0, v);
+        rb.AddForce(direcao * velocidade * Time.deltaTime,ForceMode.Impulse);
+
+        if(transform.position.y <= -10){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
